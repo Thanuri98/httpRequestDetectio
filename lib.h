19509d19a754f33001be9ea48726aa8b30abe3d8
Thanuri98 <63597680+Thanuri98@users.noreply.github.com>
2021-01-31 19:29:07 +0530

    Create lib.h

---
+++ lib.h
@@ -0,0 +1,25 @@
+#ifndef C11CODEREVIEW_LIB_H
+#define C11CODEREVIEW_LIB_H
+
+typedef enum Method {UNSUPPORTED, GET, HEAD} Method;
+
+typedef struct Header {
+    char *name;
+    char *value;
+    struct Header *next;
+} Header;
+
+typedef struct Request {
+    enum Method method;
+    char *url;
+    char *version;
+    struct Header *headers;
+    char *body;
+} Request;
+
+
+struct Request *parse_request(const char *raw);
+void free_header(struct Header *h);
+void free_request(struct Request *req);
+
+#endif //C11CODEREVIEW_LIB_H
